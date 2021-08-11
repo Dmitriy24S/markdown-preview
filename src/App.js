9b151447d74d241cbb5dc7e69b929cdff13c5a92
Dmitriy <devilins24@gmail.com>
2021-08-11 11:53:59 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import marked from "marked";
+import DOMPurify from "dompurify";
+import initialTextState from "./initial-text-state";
 
 function App() {
+  const [templateText, setTemplateText] = useState(initialTextState);
+  const markdown = marked(templateText, { breaks: true });
+  const handleChange = (e) => {
+    setTemplateText(e.target.value);
+  };
+
+  // Marked does not sanitize the output HTML
+  // DOMPurify to sanitize :
+
+  // const cleanText = DOMPurify.sanitize(templateText);
+  // const markdown = marked(cleanText, { breaks: true });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h4 className="m-4">Markdown Preview</h4>
+      <div className="row">
+        <div className="col-6">
+          <h6>Markdown:</h6>
+          <textarea
+            className="form-control p-2"
+            id="editor"
+            value={templateText}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div className="col-6">
+          <h6>Preview:</h6>
+          <div
+            className="preview rounded p-2 text-black text-left"
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: markdown }}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
